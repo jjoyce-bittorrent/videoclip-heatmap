9dedb91cf586f9cc74ad516a9a2f5c623a71fee6
Joe Joyce <jjoyce@bittorrent.com>
2016-08-31 10:22:24 -0700

    WIP: updates index of likes

--- routes/index.js
+++ routes/index.js
@@ -51,14 +51,19 @@ router.post('/api/v1/todos', function(req, res) {
 
 // hash = 2a27386e64d5a0bc42966a490b0126aff248b7feeff34446eac6bba8177a3694
 // file = Odyssey Pt. 1.mp3
-// offset_time_start
-// offset_time_end
+// likes integer[101]
 
 router.post('/api/v1/addclip', function(req, res) {
     var results = [];
 
     // Grab data from http request
-    var data = {hash: req.body.hash, file: req.body.file, offset_time_start: req.body.offset_time_start, offset_time_end: req.body.offset_time_end};
+    var data = {hash: req.body.hash, file: req.body.file, start: req.body.start, duration: req.body.duration};
+
+    var likes = new Array(101).fill(0);;
+
+    for (var i=data.start; i<Number(data.start) + Number(data.duration); i++) {
+      likes[i]=1;
+    }
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -69,31 +74,41 @@ router.post('/api/v1/addclip', function(req, res) {
           return res.status(500).json({ success: false, data: err});
         }
 
-        // SQL Query > Insert Data
-        client.query("INSERT INTO clips(hash, file, offset_time_start, offset_time_end) values($1, $2, $3, $4)", [data.hash, data.file, data.offset_time_start, data.offset_time_end]);
-
-        // SQL Query > Select Data
+        // see if there is an existing item
         var query = client.query("SELECT * FROM clips ORDER BY hash ASC");
-
-        // Stream results back one row at a time
         query.on('row', function(row) {
-            results.push(row);
+            console.log('jjoyce got a row')
+            if ((row.hash == data.hash) && (row.file == data.file)) {
+              console.log('jjoyce got a row match')
+              //update the likes array
+              for (var i=0; i<101; i++) {
+                likes[i] += row.likes[i];
+              }
+              client.query("UPDATE clips SET likes=($1) WHERE hash=($2) AND file=($3)", [likes, row.hash, row.file]);
+              results.push({hash: row.hash, file: row.file, likes: likes});
+            }
         });
 
         // After all data is returned, close connection and return results
         query.on('end', function() {
+            if (results.length == 0) {
+              client.query("INSERT INTO clips(hash, file, likes) values($1, $2, $3)", [data.hash, data.file, likes]);
+              results.push({hash: data.hash, file: data.file, likes: likes});
+            }
             done();
             return res.json(results);
         });
+
     });
 });
 
 // read
+// e.g. http://localhost:3000/api/v1/clips?id=1234567890
 router.get('/api/v1/clips', function(req, res) {
 
     var results = [];
 
-    var id = req.query.id || null;
+    var hash = req.query.hash || null;
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -106,8 +121,8 @@ router.get('/api/v1/clips', function(req, res) {
 
         // SQL Query > Select Data
         var query = client.query("SELECT * FROM clips ORDER BY hash ASC");
-        if (id) {
-          var query = client.query("SELECT * FROM clips WHERE hash=($1) ORDER BY hash ASC", [id]);
+        if (hash) {
+          var query = client.query("SELECT * FROM clips WHERE hash=($1) ORDER BY hash ASC", [hash]);
         }
 
         // Stream results back one row at a time
